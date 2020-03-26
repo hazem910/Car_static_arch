dd5fe8e33213f7602df82177a0b4fd79d7fab4de
hazem910 <hazemibrahim911@gmail.com>
2020-03-26 15:29:22 +0200

    uinttest

--- Main.c
+++ Main.c
@@ -4,19 +4,23 @@
  * Created: 2/17/2020 4:14:39 PM
  *  Author: Hazem
  */ 
-#include "..\APP\car_sm.h"
-
+#include "Macl/DIO.h"
+#include "Common/softwareDelay.h"
 int main(void)
 { 
    /*CAR_INIT_*/ 
-Car_SM_Init();
+DIO_Cfg_s ST_DIOINSTANCE={GPIOA,BIT1,OUTPUT};
+	DIO_init(&ST_DIOINSTANCE);
 /*SUPER LOOP OF THE APPLICATION */
 	while(1)
   {     
      /*CAR_LOGIC_USING_ULTRASONIC*/
-	Car_SM_Update();
+	DIO_Write(GPIOA,BIT1,HIGH);
+	softwareDelayMs(1000);
+	DIO_Write(GPIOA,BIT1,LOW);
+	softwareDelayMs(1000);
    /*SOFTWARE_DELAY*/
-   softwareDelayMs(100);
+   //ftwareDelayMs(100);
   }
 
 }
